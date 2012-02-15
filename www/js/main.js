733accf141b5fb13c5d8be76fad67e7c5559640f
Thomas Gazagnaire <thomas@gazagnaire.org>
2012-02-15 16:06:26 +0100

    Fix carousel next & prev buttons

--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,11 @@
 $(function(){
-  $(".carousel").carousel()
+  $(".carousel").carousel();
+  $(".carousel").find(".left").click(function () { $(".carousel").carousel('prev') });
+  $(".carousel").find(".right").click(function () { $(".carousel").carousel('next') });
   $("li").each(function(){
           var href = $(this).children("a").attr("href");
           if (href) {
               if (window.location.pathname.substr(-href.length) == href) {
-                  console.log("found");
                   $(this).addClass("active");
               }
           }
