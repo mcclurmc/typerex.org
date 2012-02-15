9375d55d40bb6899690e86300022a3115a99aa44
Thomas Gazagnaire <thomas@gazagnaire.org>
2012-02-15 23:32:04 +0100

    New round of edits.
    * fix color of <code> and <pre> tag
    * fix footer position
    * fix menu position
    * FAQ is now a first-class page
    * Some minor improvements to support and download
    
    => support still needs some love

--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,18 @@
+// sticky footer
+$(function(){
+  h = $(".footer").height();
+  positionFooter();
+  function positionFooter(){
+    if($(document).height() <= $(window).height()) {
+      var x = $(window).height() - h - 145;
+      $(".content").css("min-height", x + "px");
+    };
+  };
+  $(window)
+    .scroll(positionFooter)
+    .resize(positionFooter)
+});
+
 $(function(){
   $(".carousel").carousel();
   $(".carousel").find(".left").click(function () { $(".carousel").carousel('prev') });
@@ -7,8 +22,11 @@ $(function(){
           if (href) {
               if (window.location.pathname.substr(-href.length) == href) {
                   $(this).addClass("active");
+              } else {
+                  $(this).addClass("nochildren");
               }
           }
-      })
-      })
+      });
+  $(".nochildren").children("ul").hide();
+})
 
