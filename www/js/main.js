05a467770a2621643bd370f1fb73f86c21e85b1b
Thomas Gazagnaire <thomas@gazagnaire.org>
2012-02-16 10:44:20 +0100

    More fixes
    * fix active page bug
    * fix anchor bug
    * minor content updates

--- www/js/main.js
+++ www/js/main.js
@@ -13,14 +13,33 @@ $(function(){
     .resize(positionFooter)
 });
 
+// carousel
 $(function(){
   $(".carousel").carousel();
   $(".carousel").find(".left").click(function () { $(".carousel").carousel('prev') });
   $(".carousel").find(".right").click(function () { $(".carousel").carousel('next') });
+})
+
+// fix anchor issues due to fix navbar
+$(function(){
+  scrollDown();
+  function scrollDown() {
+    $("a").each(function() {
+      if ($(this).attr("name")) {
+        $(this).css("position", "relative");
+        $(this).css("top", "-80px");
+        $(this).html("&nbsp;");
+      }
+    })  
+  }
+})
+
+// active page
+$(function(){
   $("li").each(function(){
           var href = $(this).children("a").attr("href");
           if (href) {
-              if (window.location.pathname.substr(-href.length) == href) {
+              if (window.location.pathname.substr(-href.length-1) == "/"+href) {
                   $(this).addClass("active");
               } else {
                   $(this).addClass("nochildren");
